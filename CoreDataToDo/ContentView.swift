27f8a808d451c047a17a107f1c1a37e5919df87e
Louis Mbuyu <louismbuyu17@gmail.com>
2019-10-08 19:30:48 -0500

    Initial Commit

---
+++ CoreDataToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataToDo
+//
+//  Created by Louis Nelson Levoride on 08.10.19.
+//  Copyright © 2019 LouisNelson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
